c200fa2359b3e740e9caacb6a337f859620cde3f
rlabausa <rubylabausa@gmail.com>
2023-02-07 22:24:11 -0700

    Add app routes

--- cherish/src/app/app-routing.module.ts
+++ cherish/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // "first-match wins" strategy
+  // more specific routes should be placed above less specific routes
+  { path: 'map', component: MapComponent },
+  { path: '', redirectTo: '/map', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
