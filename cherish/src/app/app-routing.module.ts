c6066fa3f4c4dd5249b12b78a6b64f749f50cb78
rlabausa <rubylabausa@gmail.com>
2023-02-08 17:10:43 -0700

    Add wildcard match for 404 page

--- cherish/src/app/app-routing.module.ts
+++ cherish/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MapComponent } from './map/map.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   // "first-match wins" strategy
   // more specific routes should be placed above less specific routes
   { path: 'map', component: MapComponent },
-  { path: '', redirectTo: '/map', pathMatch: 'full' }
+  { path: '', redirectTo: '/map', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent } // Wildcard route (i.e., matches every URL) for a 404 page
 ];
 
 @NgModule({
