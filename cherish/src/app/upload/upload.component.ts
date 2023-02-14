5041d90738e2f481caf64f66a7711e13e56b0ee9
rlabausa <rubylabausa@gmail.com>
2023-02-13 22:58:28 -0700

    Set lat/long field values on location selection/marker drag

--- cherish/src/app/upload/upload.component.ts
+++ cherish/src/app/upload/upload.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { GeoSearchResult, MarkerDragResult } from '../models/leaflet-geosearch.model';
 
 @Component({
   selector: 'app-upload',
@@ -11,6 +12,14 @@ export class UploadComponent implements OnInit {
     latitude: ['', Validators.required],
     longitude: ['', Validators.required]
   });
+
+  get latitude() {
+    return this.form.get('latitude');
+  }
+
+  get longitude() {
+    return this.form.get('longitude');
+  }
 
   constructor(
     private fb: FormBuilder
@@ -26,6 +35,16 @@ export class UploadComponent implements OnInit {
     console.log('submitted')
 
     console.log(this.form)
+  }
+
+  onLocationSelected(event: GeoSearchResult) {
+    this.longitude?.setValue(event.location?.x);
+    this.latitude?.setValue(event.location?.y);
+  }
+
+  onMarkerDrag(event: MarkerDragResult) {
+    this.longitude?.setValue(event.location?.lng);
+    this.latitude?.setValue(event.location?.lat);
   }
 
 }
