68d3a211829bd0c916c4f230c6c61dceefbe28d5
rlabausa <rubylabausa@gmail.com>
2023-02-18 14:47:09 -0700

    Add placeholders for cherish data service

--- cherish/src/environments/environment.development.ts
+++ cherish/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     tileUrl: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-    nominatimApiUrl: 'https://nominatim.openstreetmap.org'
+    nominatimApiUrl: 'https://nominatim.openstreetmap.org',
+    cherishDataApiUrl: ''
 };
