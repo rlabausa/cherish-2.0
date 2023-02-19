9cc2ab9e91bfe812a26f9499d60b77334bc9e4e3
rlabausa <rubylabausa@gmail.com>
2023-02-19 15:35:14 -0700

    Scaffold spring boot data service with start.spring.io

---
+++ cherish_service/src/main/java/com/rlabausa/cherish_service/CherishServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rlabausa.cherish_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherishServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherishServiceApplication.class, args);
+	}
+
+}
---
+++ cherish_service/src/test/java/com/rlabausa/cherish_service/CherishServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlabausa.cherish_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherishServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
