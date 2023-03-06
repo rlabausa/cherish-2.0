3f39308756e6a965e603bfe0f6cec6b45f3a4db8
rlabausa <rubylabausa@gmail.com>
2023-03-05 20:46:38 -0700

    Refactor photo controller

--- cherish_service/src/main/java/com/rlabausa/cherish_service/controllers/PhotosController.java
+++ cherish_service/src/main/java/com/rlabausa/cherish_service/controllers/PhotosController.java
@@ -2,13 +2,15 @@ package com.rlabausa.cherish_service.controllers;
 
 import com.rlabausa.cherish_service.models.Photo;
 import com.rlabausa.cherish_service.repositories.PhotoRepository;
+import com.rlabausa.cherish_service.services.PhotoService;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,53 +18,33 @@ import java.nio.file.Paths;
 @RestController
 @RequestMapping("photos")
 public class PhotosController {
-    private final Path FOLDER_PATH = Paths.get("C:", "Users", "rubyl", "Desktop", "cherish_photos");
-    private PhotoRepository photoRepository;
 
-    public PhotosController(PhotoRepository photoRepository){
+    private PhotoRepository photoRepository;
+    private PhotoService photoService;
+
+    public PhotosController(PhotoRepository photoRepository, PhotoService photoService){
         this.photoRepository = photoRepository;
+        this.photoService = photoService;
     }
 
     @PostMapping()
     public Photo addPhoto(@RequestParam MultipartFile file) throws IOException {
-
-        String filePath = this.generateFilePathForStorage(file);
-
-        // save to database
-        Photo newPhoto = new Photo();
-        newPhoto.setFilePath(filePath);
-        Photo savedPhoto = this.photoRepository.save(newPhoto);
-
-        if (savedPhoto != null){
-            // save to filesystem
-            Files.copy(
-                    file.getInputStream(),
-                    this.FOLDER_PATH.resolve(file.getOriginalFilename())
-            );
-        }
-
-        return savedPhoto;
-
+        return this.photoService.addPhoto(file);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getPhoto(@PathVariable Long id) throws IOException {
-        Photo photo = this.photoRepository.findById(id)
-                .orElseThrow();
-
-        String photoPath = photo.getFilePath();
-
-        Path file = new File(photoPath).toPath();
-        byte [] bytes = Files.readAllBytes(file);
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.IMAGE_PNG)
-                .body(bytes);
+        return this.photoService.getPhoto(id);
     }
 
-    private String generateFilePathForStorage(MultipartFile file){
-        String newFileName = file.getOriginalFilename();
-        Path filePath = this.FOLDER_PATH.resolve(newFileName);
-        return filePath.toString();
+
+    @GetMapping(
+            value = "/src/{id}",
+            produces = MediaType.IMAGE_PNG_VALUE
+    )
+    public ResponseEntity<?> getPhotoSrc(@PathVariable Long id) throws IOException {
+        return this.photoService.downloadPhoto(id);
     }
+
+
 }
